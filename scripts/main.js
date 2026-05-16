4ad63e71a0e1e5a764c58f26001a0f39bff4be08
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-05-16 14:31:02 +0300

    added files

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// let a = Number(prompt("Enter number 1: "));
+// let b = Number(prompt("Enter number 2: "));
+
+// if (typeof(a) == 'object')
+// if (isNaN(a) || isNaN(b)) {
+//     alert("Error");
+// } else {
+//     alert (a+b);
+// }
+
+
+
